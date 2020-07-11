b5ea41759d23c6c7fc91170520bcf32b4642e5a6
zuniar.hidaya <zuniar.hidaya@gmail.com>
2020-07-11 20:35:46 +0700

    nav non-alarm

--- app/src/main/java/com/example/scheduling_activity/MainActivity.java
+++ app/src/main/java/com/example/scheduling_activity/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_profile,
-                R.id.nav_home, R.id.nav_alarm, R.id.nav_dss)
+                R.id.nav_home, R.id.nav_dss)
                 .setDrawerLayout(drawer)
                 .build();
 
