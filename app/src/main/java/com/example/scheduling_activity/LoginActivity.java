260f3f7aa9141dfc13c5f243db52d1372ea042af
Naufal <naufalmahfudzismail@gmail.com>
2020-07-14 05:46:34 +0700

    logo, splash

--- app/src/main/java/com/example/scheduling_activity/LoginActivity.java
+++ app/src/main/java/com/example/scheduling_activity/LoginActivity.java
@@ -23,12 +23,8 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
-                onBackPressed();
             }
         });
     }
 
-    public void onBackPressed(){
-
-    }
 }
